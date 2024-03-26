8a07aa314eb0364d6a1c64806b5e466db0d5e37b
tarunbapat <tarun.bapat710@gmail.com>
2024-03-26 23:37:51 +0530

    redux saga

---
+++ redux-saga/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <>djfh</>;
+}
+
+export default App;
+
+// https://najm-eddine-zaga.medium.com/react-redux-toolkit-with-redux-saga-a1439075743d#:~:text=Redux%20Thunk%20is%20easy%20to,sequence%20of%20steps%2C%20called%20sagas.
