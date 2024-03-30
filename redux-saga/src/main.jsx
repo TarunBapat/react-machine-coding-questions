a63089e6ccfc8fbb2c96a16ad90d900e60960d85
tarunbapat <tarun.bapat710@gmail.com>
2024-03-30 23:56:31 +0530

    redux-saga-1

--- redux-saga/src/main.jsx
+++ redux-saga/src/main.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import createSagaMiddleware from "redux-saga";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { logger } from "redux-logger";
+import reducer from "./reducers";
 import App from "./App";
-import store from "./store/index";
+import rootSaga from "./sagas";
+const sagaMiddleware = createSagaMiddleware();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+const store = createStore(reducer, applyMiddleware(sagaMiddleware, logger));
+sagaMiddleware.run(rootSaga);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
+if (module.hot) {
+  module.hot.accept(App);
+}
--- redux-saga/src/store/index.js
+++ redux-saga/src/store/index.js
@@ -1,12 +1,12 @@
-import createSagaMiddleware from "@redux-saga/core";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducers from "../redux/root-reducer";
+// import createSagaMiddleware from "@redux-saga/core";
+// import { configureStore } from "@reduxjs/toolkit";
+// import rootReducers from "../redux/root-reducer";
 
-const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
-const store = configureStore({
-  reducer: rootReducers,
-  middleware: [sagaMiddleware],
-});
+// const store = configureStore({
+//   reducer: rootReducers,
+//   middleware: [sagaMiddleware],
+// });
 
-export default store;
+// export default store;
